eb6dcf52fdc4eeda355b97373264d9f79a42e946
Dhairya Majmudar <124715224+DhairyaMajmudar@users.noreply.github.com>
2025-06-26 22:41:58 +0530

    kro-cli add validate command (#527)

--- cmd/kro/commands/validate.go
+++ cmd/kro/commands/validate.go
@@ -16,27 +16,75 @@ package commands
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/kro-run/kro/api/v1alpha1"
+	kroclient "github.com/kro-run/kro/pkg/client"
+	"github.com/kro-run/kro/pkg/graph"
 	"github.com/spf13/cobra"
+	"sigs.k8s.io/yaml"
 )
 
 var validateCmd = &cobra.Command{
 	Use:   "validate",
 	Short: "Validate the ResourceGraphDefinition",
-	Long: `Validate the ResourceGraphDefinition. This command checks 
-	if the ResourceGraphDefinition is valid and can be used to create a ResourceGraph.`,
+	Long: `Validate the ResourceGraphDefinition. This command checks ` +
+		`if the ResourceGraphDefinition is valid and can be used to create a ResourceGraph.`,
+}
+
+var resourceGroupDefinitionFile string
+
+func init() {
+	validateRGDCmd.PersistentFlags().StringVarP(&resourceGroupDefinitionFile, "file", "f", "",
+		"Path to the ResourceGroupDefinition file")
 }
 
 var validateRGDCmd = &cobra.Command{
 	Use:   "rgd [FILE]",
 	Short: "Validate a ResourceGraphDefinition file",
-	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// TODO(DhairyaMajmudar): Implement the logic to validate the ResourceGraphDefinition file
+		if resourceGroupDefinitionFile == "" {
+			return fmt.Errorf("ResourceGroupDefinition file is required")
+		}
 
-		fmt.Println("Validation not implemented yet")
+		data, err := os.ReadFile(resourceGroupDefinitionFile)
+		if err != nil {
+			return fmt.Errorf("failed to read ResourceGroupDefinition file: %w", err)
+		}
+
+		var rgd v1alpha1.ResourceGraphDefinition
+		if err = yaml.Unmarshal(data, &rgd); err != nil {
+			return fmt.Errorf("failed to unmarshal ResourceGroupDefinition: %w", err)
+		}
+
+		if err := validateRGD(&rgd); err != nil {
+			return fmt.Errorf("validation failed: %w", err)
+		}
+
+		fmt.Println("Validation successful! The ResourceGraphDefinition is valid.")
 		return nil
 	},
+}
+
+func validateRGD(rgd *v1alpha1.ResourceGraphDefinition) error {
+	set, err := kroclient.NewSet(kroclient.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to create client set: %w", err)
+	}
+
+	restConfig := set.RESTConfig()
+
+	builder, err := graph.NewBuilder(restConfig)
+	if err != nil {
+		return fmt.Errorf("failed to create graph builder: %w", err)
+	}
+
+	_, err = builder.NewResourceGraphDefinition(rgd)
+	if err != nil {
+		return fmt.Errorf("failed to create ResourceGraphDefinition: %w", err)
+	}
+
+	return nil
 }
 
 func AddValidateCommands(rootCmd *cobra.Command) {
